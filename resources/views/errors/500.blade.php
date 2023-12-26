19c06ed2e228e5cf850a468eb9f88e17bcea4e7c
Kepodes <kepodev3@gmail.com>
2023-12-26 19:17:00 +0700

    MINOR

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        poppins: ['Poppins', 'sans-serif']
+                    },
+                }
+            }
+        }
+        </script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap">
+    <title>Error!!</title>
+</head>
+<body class="bg-[rgba(27,33,44,255)]" >
+    <div class="flex flex-col h-[100vh] justify-center items-center">
+        <img class="w-40 h-auto mb-5" src="/image/kabel.png" alt="gambar kabel">
+        <h1 class="justify-self-center text-gray-400 font-poppins font-bold text-3xl">WADUH KAMU APAIN KAK? X_X</h1>
+        <p class="text-gray-500 font-poppins font-semibold" >JANGAN DI RUSAK YA KAK!!</p>
+
+        <p class="text-gray-500 font-poppins font-semibold" >Laporkan Website Down!! <a class="text-blue-600" target="_blank" href="https://wa.me/6289633831593?text=BANG+ERROR+500+BANG!!+.">Click here!!</a></p>
+    </div>
+</body>
+</html>
